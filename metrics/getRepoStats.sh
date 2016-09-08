38c1472a782471c3567372b93a40d3fdb9ec3f3a
Cameron Smith <smithc11@rpi.edu>
2016-09-07 20:38:45 -0400

    github api calls to collect usage stats
    requires jq (https://stedolan.github.io/jq/) and curl

---
+++ metrics/getRepoStats.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+jq=/path/to/jq/binary
+auth='Authorization: token <your OAuth token>'
+media='Accept:   application/vnd.github.spiderman-preview'
+repourl=https://api.github.com/repos/SCOREC/EnGPar
+
+stamp=`date +%d%m%Y`
+
+#views
+curl $repourl/traffic/views -H "$auth" -H "$media" > $stamp.views
+count=$($jq -r '.count' $stamp.views)
+unique=$($jq -r '.uniques' $stamp.views)
+echo $stamp $count $unique >> core.views
+
+#clones
+curl $repourl/traffic/clones -H "$auth" -H "$media" > $stamp.clones
+count=$($jq -r '.count' $stamp.clones)
+unique=$($jq -r '.uniques' $stamp.clones)
+echo $stamp $count $unique >> core.clones
+
+#send a stats request to ensure stats have been compiled
+curl $repourl/stats/commit_activity -H "$auth" > /dev/null
+sleep 60 # give github time to compile stats
+
+#weekly commits - collect previous weeks stats on a monday
+dayofweek=$(date +%u)
+if [ $dayofweek = 1 ]; then # day 1 is monday
+  curl $repourl/stats/commit_activity -H "$auth" > $stamp.commits
+  count=$($jq -r ".[50].total" $stamp.commits) # week 50 is the previous week
+  echo $stamp $count >> core.commits
+fi
+
+#unique committers
+curl $repourl/stats/contributors -H "$auth" > $stamp.contributors
+count=$($jq -r ".[].total" $stamp.contributors | wc -l)
+echo $stamp $count >> core.contributors
+
+#pull requests
+curl $repourl/pulls?state=closed -H "$auth" > $stamp.pulls
+count=$($jq -r ".[].id" $stamp.pulls | wc -l)
+echo $stamp $count >> core.pulls
