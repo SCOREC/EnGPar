8a7b4da0c9aa298c27d18988450335c55be968b2
Gerrett Diamond <diamog@rpi.edu>
2017-01-19 14:37:06 -0500

    Some regression testing added requires cube mesh and the google binary graph

--- config.sh
+++ config.sh
@@ -1,8 +1,12 @@
 
 cmake .. \
-      -DCMAKE_C_COMPILER="mpicc" \
-      -DCMAKE_C_FLAGS="-g" \
-      -DCMAKE_CXX_COMPILER="mpicxx" \
-      -DCMAKE_CXX_FLAGS="-g -std=c++11" \
-      -DENABLE_ZOLTAN=ON \
-      -DSCOREC_PREFIX=/lore/diamog/core/installJessie
+    -DCMAKE_C_COMPILER="mpicc" \
+    -DCMAKE_C_FLAGS="-g" \
+    -DCMAKE_CXX_COMPILER="mpicxx" \
+    -DCMAKE_CXX_FLAGS="-g -std=c++11" \
+    -DENABLE_ZOLTAN=ON \
+    -DSCOREC_PREFIX=/lore/diamog/core/installJessie \
+    -DIS_TESTING=ON \
+    -DMESHES=/users/diamog/meshes/ \
+    -DGRAPHS=/users/diamog/graphs/
+
---
+++ test/testing.cmake
@@ -0,0 +1,49 @@
+set(MESHES ""
+    CACHE STRING
+    "Extracted http://scorec.rpi.edu/pumi/pumi_test_meshes.tar.gz")
+set(MESHES ""
+    CACHE STRING
+    "Extracted http://scorec.rpi.edu/pumi/pumi_test_meshes.tar.gz")
+
+function(mpi_test TESTNAME PROCS EXE)
+  add_test(
+    NAME ${TESTNAME}
+    COMMAND ${MPIRUN} ${MPIRUN_PROCFLAG} ${PROCS} ${VALGRIND} ${VALGRIND_ARGS} ${EXE} ${ARGN}
+  )
+endfunction(mpi_test)
+
+mpi_test(buildSerialMeshGraph32 1
+  ./buildMeshGraph
+  "${MESHES}/cube/cube.dmg"
+  "${MESHES}/cube/pumi11/cube.smb"
+  3
+  2)
+
+mpi_test(buildSerialMeshGraph31 1
+  ./buildMeshGraph
+  "${MESHES}/cube/cube.dmg"
+  "${MESHES}/cube/pumi11/cube.smb"
+  3
+  1)
+
+mpi_test(buildParallelMeshGraph32 2
+  ./buildMeshGraph
+  "${MESHES}/cube/cube.dmg"
+  "${MESHES}/cube/pumi670/2/cube.smb"
+  3
+  2)
+
+mpi_test(buildParallelMeshGraph31 4
+  ./buildMeshGraph
+  "${MESHES}/cube/cube.dmg"
+  "${MESHES}/cube/pumi670/4/cube.smb"
+  3
+  1)
+
+mpi_test(buildSerialBinaryGraph 1
+  ./buildBinaryGraph
+  "${GRAPHS}/google.ebin")
+
+mpi_test(buildParallelBinaryGraph 4
+  ./buildBinaryGraph
+  "${GRAPHS}/google.ebin")
