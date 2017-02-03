b1c306528e423e6fce531e1ac1c7d7f5b6fb47d7
Gerrett <diamog@rpi.edu>
2017-02-02 19:14:24 -0500

    Add cmake function to build tests

--- config.sh
+++ config.sh
@@ -3,11 +3,11 @@ cmake .. \
     -DCMAKE_C_FLAGS="-g" \
     -DCMAKE_CXX_COMPILER="mpicxx" \
     -DCMAKE_CXX_FLAGS="-g -std=c++11 -Wl,--no-as-needed -ldl -pthread" \
-    -DENABLE_ZOLTAN=ON \
-    -DSCOREC_PREFIX=/path/to/core/install \
+    -DENABLE_ZOLTAN=OFF \
+    -DSCOREC_PREFIX=~/Documents/scorec/core/install \
     -DENABLE_KOKKOS=ON \
-    -DKOKKOS_PREFIX=/path/to/kokkos_install \
-    -DIS_TESTING=ON \
+    -DKOKKOS_PREFIX=~/Documents/scorec/kokkos_install \
+    -DIS_TESTING=OFF \
     -DMESHES=/path/to/meshes/ \
     -DGRAPHS=/path/to/graphs/
 
