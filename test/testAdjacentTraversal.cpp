7a45d5276fc127a31490e71c981f456b8216d8d3
Gerrett <diamog@rpi.edu>
2017-02-14 16:58:41 -0500

    Add comparison of traversal modes

--- test/testAdjacentTraversal.cpp
+++ test/testAdjacentTraversal.cpp
@@ -6,9 +6,9 @@
 #include <apfMDS.h>
 #include <binGraph.h>
 #include <apf.h>
-#include <set>
+#include <vector>
 void testAdjacent(agi::Ngraph* g,agi::etype t=0);
-
+void compareTraversal(agi::Ngraph* g,agi::etype t=0);
 int main(int argc, char* argv[]) {
   MPI_Init(&argc,&argv);
   PCU_Comm_Init();
@@ -22,16 +22,21 @@ int main(int argc, char* argv[]) {
 
   //run the adjacency test
   testAdjacent(g);
+  compareTraversal(g);
   
   //Destroy Ngraph
   agi::destroyGraph(g);
 
   //Construct Ngraph with edges over mesh vertices
-  g = agi::createAPFGraph(m,3,1);
+  int secondaries[2] = {0,1};
+  g = agi::createAPFGraph(m,3,secondaries,2);
 
   //run the adjacency test
-  testAdjacent(g);
-
+  for (int i=0;i<2;i++) {
+    testAdjacent(g,secondaries[i]);
+    compareTraversal(g,secondaries[i]);
+  }
+  
   //Destroy Ngraph
   agi::destroyGraph(g);
 
@@ -44,6 +49,7 @@ int main(int argc, char* argv[]) {
 
   //run the adjacency test
   testAdjacent(g);
+  compareTraversal(g);
 
   //Destroy the graph
   agi::destroyGraph(g);
@@ -92,4 +98,52 @@ void testAdjacent(agi::Ngraph* g,agi::etype t) {
   else {
     assert(num_edges==g->numLocalEdges(t));
   }
+}
+
+void compareTraversal(agi::Ngraph* g,agi::etype t) {
+  if (!PCU_Comm_Self())
+    printf("Beginning Comparison\n");
+  agi::VertexIterator* vitr = g->begin();
+  agi::GraphVertex* vtx = NULL;
+  int num_pins =0;
+  int num_edges=0;
+  int tot_degrees=0;
+  std::vector<agi::GraphEdge*> edges;
+  std::vector<agi::GraphVertex*> vtxs;
+  while ((vtx = g->iterate(vitr))) {
+    agi::GraphIterator* gitr = g->adjacent(vtx,t);
+    agi::GraphVertex* other = NULL;
+    agi::GraphEdge* edge = NULL;
+    while ((other = g->iterate(gitr))) {
+      edges.push_back(g->edge(gitr));
+      vtxs.push_back(other);
+    }
+    g->destroy(gitr);
+  }
+  vitr = g->begin();
+  int i=0;
+  while ((vtx = g->iterate(vitr))) {
+    agi::EdgeIterator* eitr = g->edges(vtx,t);
+    agi::GraphVertex* other;
+    agi::GraphEdge* edge;
+    while ((edge = g->iterate(eitr))) {
+      assert(edges[i]==edge);
+      if (g->isHyper()) {
+	agi::PinIterator* pitr = g->pins(edge);
+	for (agi::lid_t j=0;j<g->degree(edge);j++) {
+	  other = g->iterate(pitr);
+	  assert(g->isEqual(other,vtxs[i]));
+	  assert(edges[i]==edge);
+	  i++;
+	}
+      }
+      else {
+	assert(g->v(edge)==vtxs[i]);
+	i++;
+      }
+    }
+    g->destroy(eitr);
+  }
+  assert(i==vtxs.size());
+  assert(i==edges.size());
 }
