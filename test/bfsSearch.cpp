4ac8a44c49714db1f754bb3d5b625ec27d24abd0
Andrey Choi <Andrey.Gregory.Choi@gmail.com>
2017-03-21 09:20:34 -0400

    Minor changes to the bfsTraditional function to make it cleaner

--- test/bfsSearch.cpp
+++ test/bfsSearch.cpp
@@ -8,32 +8,34 @@
 void bfsTraditional(agi::Ngraph* g, agi::etype t=0){
   //The structures for passing over the graph
   std::set<agi::GraphVertex*> visited; //A set to hold all of the visited verticies
-  std::vector<agi::GraphVertex*> curVector; //The vector of the vertecies currently being visited over
-  std::vector<agi::GraphVertex*> nextVector; //The vector of the verticies adjacent to the currently visited verticies
-  //Iterator for starting the graph traversal
-  agi::VertexIterator* vitr = g->begin();
-  agi::GraphVertex* curVtx = (g->iterate( vitr ));
-  curVector.push_back(curVtx);
-  while( !curVector.empty() ){
-    nextVector.clear();
-    std::vector<agi::GraphVertex*>::iterator curVtxIt;
-    int sizeOfVector = (int) curVector.size();
+  std::vector<agi::GraphVertex*> curVtxVector; //The vector of the vertecies currently being visited over
+  std::vector<agi::GraphVertex*> nextVtxVector; //The vector of the verticies adjacent to the currently visited verticies
+
+  agi::VertexIterator* vitr = g->begin(); //Iterator for starting the graph traversal
+  agi::GraphVertex* curVtx = (g->iterate(vitr)); //Gets the first vertex of the graph from the iterator
+  curVtxVector.push_back(curVtx);
+  while( !curVtxVector.empty() ){
+    nextVtxVector.clear();
+    int sizeOfVector = (int) curVtxVector.size();
     for( int i=0; i < sizeOfVector; ++i ){
-      curVtx = curVector[i];
-    //for( curVtxIt=curVector.begin(); curVtxIt != curVector.end(); ++curVtxIt ){
-    //  curVtx = *(curVtxIt);
+      curVtx = curVtxVector[i];
+      //Checks if the current vertex has been visited before
       std::set<agi::GraphVertex*>::iterator existVtx = visited.find(curVtx);
       if( existVtx != visited.end() ){
         continue;
       }
+
+      //Adds the neighbors of the current vertex to the vector of the next group of vertices to visit 
+      //and adds the current vertex to visited
       agi::GraphIterator* neighbor = g->adjacent(curVtx,t);
       agi::GraphVertex* neighborVtx;
       while( neighborVtx = g->iterate(neighbor) ){
-        nextVector.push_back(neighborVtx);
+        nextVtxVector.push_back(neighborVtx);
       }
       visited.insert(curVtx);
     }
-    curVector = nextVector;
+
+    curVtxVector = nextVtxVector;
   }
 }
 
