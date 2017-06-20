2f8197ded8f58e1b0d58571a831d4841f55b8d42
Gerrett <diamog@rpi.edu>
2017-06-20 09:21:28 -0400

    reduce old constructGraph api to a combination of the new set

--- agi/ngraph.cpp
+++ agi/ngraph.cpp
@@ -53,123 +53,10 @@ void Ngraph::constructGraph(bool isHG,
       sprintf(message,"%sCreating traditional graph\n",message);
     EnGPar_Log_Function(message);
   }
-  destroyData();
-  isHyperGraph=isHG;
-  num_local_verts=verts.size();
-  local_unmap = new gid_t[num_local_verts];
-  local_weights = new wgt_t[num_local_verts];
-  local_coords = NULL;
-  num_types = 0;
-  etype t = addEdgeType();
-  degree_list[t] = new lid_t[num_local_verts+1];
-  degree_list[t][0] = 0;
-  for (lid_t i=0;i<verts.size();i++) {
-    local_unmap[i] = verts[i];
-    vtx_mapping[verts[i]]=i;
-    degree_list[t][i+1]=0;
-    //set local_weights
-    if (wgts.size()>i)
-      local_weights[i] = wgts[i];
-    else
-      local_weights[i] = 1;
-    //set local coords
-  }
-
-  num_ghost_verts=0;
-  num_local_edges[t] = edge_ids.size();
-  num_local_pins[t] = pins_to_verts.size();
-  edge_weights[t] = NULL;
-  edge_unmap[t] = new gid_t[edge_ids.size()];
-  pin_degree_list[t] = new lid_t[degs.size()+1];
-  pin_degree_list[t][0]=0;
-  pin_list[t] = new lid_t[pins_to_verts.size()];
-  for (lid_t i=0;i<edge_ids.size();i++) {
-    //set edge_weight
-    gid_t gid = edge_ids[i];
-    edge_mapping[t][gid]=i;
-    edge_unmap[t][i]=gid;
-    pin_degree_list[t][i+1]=pin_degree_list[t][i]+degs[i];
-    for (lid_t j=pin_degree_list[t][i];j<pin_degree_list[t][i+1];j++) {
-      gid_t v = pins_to_verts[j];
-      map_t::iterator vitr = vtx_mapping.find(v);
-      if (vitr!=vtx_mapping.end()&&vitr->second<num_local_verts) {
-	if (!(j%2))
-	  degree_list[t][vitr->second+1]++;
-	else if (isHyperGraph)
-	  degree_list[t][vitr->second+1]++;
-	pin_list[t][j]=vitr->second;
-      }
-      else {
-	if (vitr==vtx_mapping.end()) {
-	  vtx_mapping[v]=num_local_verts+num_ghost_verts;
-	  pin_list[t][j] = num_local_verts+num_ghost_verts++;
-	}
-	else {
-	  pin_list[t][j]=vitr->second;
-	}
-      }
-    }
-  }
-  if (!isHyperGraph)
-    num_local_edges[t] = num_local_pins[t]/2;
-  for (lid_t i=1;i<num_local_verts+1;i++) {
-    degree_list[t][i]+=degree_list[t][i-1];
-  }
-  uint64_t* temp_counts = (uint64_t*)malloc(num_local_verts*sizeof(uint64_t));
+  constructVerts(isHG,verts,wgts);
+  constructEdges(edge_ids,degs,pins_to_verts);
+  constructGhosts(owns);
   
-  std::memcpy(temp_counts, degree_list[t], num_local_verts*sizeof(uint64_t));
-  edge_list[t] = new lid_t[degree_list[t][num_local_verts]];
-  ghost_unmap=NULL;
-  owners=NULL;
-  if (num_ghost_verts>0) {
-    ghost_unmap = new lid_t[num_ghost_verts];
-    owners = new part_t[num_ghost_verts];
-  }
-  
-  for (lid_t i=0;i<edge_ids.size();i++) {
-    for (lid_t j=pin_degree_list[t][i];j<pin_degree_list[t][i+1];j++) {
-      lid_t u = pin_list[t][j];
-      if (u>=num_local_verts){
-	ghost_unmap[u-num_local_verts] = pins_to_verts[j];
-	owners[u-num_local_verts] = owns[pins_to_verts[j]];
-      }
-      if (isHyperGraph) {
-	if (u<num_local_verts)
-	  edge_list[t][temp_counts[u]++] = i;
-      }
-      else {
-	lid_t v = pin_list[t][++j];
-	if (v>=num_local_verts){
-	  ghost_unmap[v-num_local_verts] = pins_to_verts[j];
-	  owners[v-num_local_verts] = owns[pins_to_verts[j]];
-	}
-	if (u<num_local_verts)
-	  edge_list[t][temp_counts[u]++] = v;
-      }
-    }
-  }
-  free(temp_counts);
-  //Setup global counters
-  num_global_verts = PCU_Add_Long(num_local_verts);
-
-  gid_t nOwnedEdges=num_local_edges[t];
-  if (isHyperGraph) {
-    EdgeIterator* eitr = begin(t);
-    GraphEdge* edge;
-    while ((edge = iterate(eitr))) {
-      Peers neighbors;
-      getResidence(edge,neighbors);
-      Peers::iterator itr;
-      for (itr = neighbors.begin();itr!=neighbors.end();itr++) {
-        if (*itr<PCU_Comm_Self()) {
-          --nOwnedEdges;
-          break;
-        }
-      }
-    }
-  }
-  num_global_edges[t] = PCU_Add_Long(nOwnedEdges);
-  num_global_pins[t] = PCU_Add_Long(num_local_pins[t]);
   if (EnGPar_Is_Log_Open()) {
     EnGPar_End_Function();
   }
@@ -207,12 +94,20 @@ void Ngraph::constructVerts(bool isHG,
 
   num_ghost_verts=0;
   num_global_verts = PCU_Add_Long(num_local_verts);
+  if (EnGPar_Is_Log_Open()) {
+    EnGPar_End_Function();
+  }
 
 }
 
 etype Ngraph::constructEdges(std::vector<gid_t>& edge_ids,
                              std::vector<lid_t>& degs,
                              std::vector<gid_t>& pins_to_verts) {
+  if (EnGPar_Is_Log_Open()) {
+    char message[45];
+    sprintf(message,"constructEdges\n");
+    EnGPar_Log_Function(message);
+  }
   etype t = addEdgeType();
   degree_list[t] = new lid_t[num_local_verts+1];
   for (lid_t i=0;i<num_local_verts+1;i++)
@@ -287,11 +182,20 @@ etype Ngraph::constructEdges(std::vector<gid_t>& edge_ids,
     }
   }
   free(temp_counts);
+  if (EnGPar_Is_Log_Open()) {
+    EnGPar_End_Function();
+  }
 
   return t;
 }
 
 void Ngraph::constructGhosts(std::unordered_map<gid_t,part_t>& owns) {
+  if (EnGPar_Is_Log_Open()) {
+    char message[45];
+    sprintf(message,"constructGhosts\n");
+    EnGPar_Log_Function(message);
+  }
+
   owners = NULL;
   if (num_ghost_verts>0)
     owners = new part_t[num_ghost_verts];
@@ -320,6 +224,10 @@ void Ngraph::constructGhosts(std::unordered_map<gid_t,part_t>& owns) {
 
     }
   }
+  if (EnGPar_Is_Log_Open()) {
+    EnGPar_End_Function();
+  }
+
 }
 
 Ngraph::~Ngraph() {
