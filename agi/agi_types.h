6510a9198c338eed4f3a9e60f7cc202df420f6fc
Cameron Smith <smithc11@rpi.edu>
2017-07-18 16:41:03 -0400

    set the type sizes in a header for fortran and C

---
+++ agi/agi_types.h
@@ -0,0 +1,13 @@
+#ifndef AGI_TYPES_H
+#define AGI_TYPES_H
+
+#define AGI_GID_T   int64_t
+#define AGI_GID_FT  C_INT64_T
+#define AGI_LID_T   int64_t
+#define AGI_LID_FT  C_INT64_T
+#define AGI_WGT_T   double
+#define AGI_WGT_FT  C_DOUBLE
+#define AGI_PART_T  int32_t
+#define AGI_PART_FT C_INT32_T
+
+#endif
--- test/ftnTest.f90
+++ test/ftnTest.f90
@@ -1,13 +1,18 @@
 program main
   use engpar
+  use iso_c_binding
   implicit none
   include 'mpif.h'
+#include "../agi/agi_types.h"
 
   integer :: ierr
+  integer(AGI_GID_FT) :: gid
+  type(c_ptr) :: graph
   call mpi_init(ierr)
   call cengpar_initialize()
   call cengpar_setftncommunicator(MPI_COMM_WORLD)
   write (*,'(a)' ) 'Hello EnGPar!'
+  write (*,* ) sizeof(gid)
   call cengpar_finalize()
   call mpi_finalize(ierr)
   stop
