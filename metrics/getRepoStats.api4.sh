b4cf52cc9387c5dd2641d839c8cf7ee7c15ef347
Cameron Smith <smithc11@rpi.edu>
2017-09-26 13:01:31 -0400

    metrics: collects monthly commits using api4

--- metrics/getRepoStats.api4.sh
+++ metrics/getRepoStats.api4.sh
@@ -56,3 +56,56 @@ EOF
 
 $jq '.data.repository.pullRequests.edges[][]["updatedAt"]' $stamp.pulls | xargs -L 1 date +"%d-%m-%Y" -d > engpar.pulls
 sort -t '-' -k 2 -k 1 engpar.pulls > engpar.sorted.pulls
+
+#commits - this assumes that there are less than 100 commits per month
+for year in 2015 2016 2017; do
+  for i in {1..11}; do 
+    echo $i
+    printf -v mon "%02g" $i
+    printf -v nextmon "%02g" $((i+1))
+    echo 'mon' $mon 'nextmon' $nextmon
+    log=$stamp.commits${year}.${mon}-${nextmon}
+    curl https://api.github.com/graphql \
+      -H "$auth" \
+      -X POST \
+      -o $log \
+      -d @- << EOF
+    {
+      "query": "query {
+         repository(owner:\"SCOREC\", name:\"EnGPar\") {
+          ref(qualifiedName:\"master\") {
+            name
+            target {
+              ... on Commit {
+                history(first:100, since:\"${year}-${mon}-01T00:00:00Z\", until:\"${year}-${nextmon}-01T00:00:00Z\") {
+                  edges {
+                    node {
+                      oid
+                      message
+                      author {
+                        name
+                        email
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }"
+    }
+EOF
+    
+    $jq '.data.repository.ref.target.history.edges[][]["author"]["name"]' $log > $log.name
+    if [ -s $log.name ]; then
+      wc -l $log.name
+      sort -u $log.name
+    else
+      echo "$log.name is empty"
+      rm $log $log.name
+    fi
+  
+  done
+done
