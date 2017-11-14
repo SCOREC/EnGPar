ab041ed4302bf03154a18f01d0df735bb148c2e0
Gerrett <diamog@rpi.edu>
2017-11-14 13:09:58 -0700

    Cleanup standard config file

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 cmake .. \
     -DCMAKE_C_COMPILER="mpicc" \
-    -DCMAKE_C_FLAGS="-g" \
+    -DCMAKE_C_FLAGS="" \
     -DCMAKE_CXX_COMPILER="mpicxx" \
-    -DCMAKE_CXX_FLAGS="-g -std=c++11 -Wl,--no-as-needed -ldl -pthread" \
+    -DCMAKE_CXX_FLAGS="-std=c++11" \
     -DENABLE_ZOLTAN=OFF \
     -DENABLE_PUMI=OFF \
     -DSCOREC_PREFIX=/path/to/core/install \
