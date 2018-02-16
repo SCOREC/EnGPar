b25752727de72a5f581978c0ec37684b80631653
Gerrett Diamond <diamog@rpi.edu>
2018-02-16 14:38:34 -0500

    Fix compiler errors for ssg code

---
+++ ssg/ssg_types.h
@@ -0,0 +1,14 @@
+#ifndef SSG_TYPES__
+#define SSG_TYPES__
+
+#include <ngraph.h>
+
+namespace ssg {
+
+  typedef agi::lid_t lid_t;
+  typedef agi::gid_t gid_t;
+  typedef agi::part_t part_t;
+  typedef agi::etype etype;
+}
+
+#endif
