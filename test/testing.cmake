9186914ef63a6a81cd009eb80fbfd76a4cac8f4a
Cameron Smith <smithc11@rpi.edu>
2018-02-16 20:01:13 -0500

    ssg: add line test

--- test/testing.cmake
+++ test/testing.cmake
@@ -134,6 +134,7 @@ mpi_test(testDistanceQueueAero81334 1
 mpi_test(testDistanceQueueAero1457 1
   ./testDistanceQueue "${GRAPHS}/aero1Belm/afterMigr_1457")
 
+mpi_test(ngraphToSsgLine 1 ./ngraphToSsg)
 mpi_test(ngraphToSsgAero25114 1
   ./ngraphToSsg "${GRAPHS}/aero1Belm/graph128Ki_25114")
 
