e9598e91fc83a164b92a60a43de14943419b2457
Gerrett Diamond <diamog@rpi.edu>
2018-06-05 16:13:21 -0400

    cmake variables to control big endian/index size builds

--- agi/agi_typeconvert.cpp
+++ agi/agi_typeconvert.cpp
@@ -7,6 +7,12 @@ namespace agi {
 
   lid_t fromPtr(GraphVertex* v) {
     lid_t* lid = reinterpret_cast<lid_t*>((char*)(&v));
+#ifndef ENGPAR_BIG_ENDIAN
     return lid[0]-1;
+#elif LOCAL_ID_TYPE == int64_t
+    return lid[0]-1;
+#else
+    return lid[1]-1;
+#endif
   }
 }
--- agi/agi_types.h
+++ agi/agi_types.h
@@ -3,13 +3,13 @@
 
 #define AGI_GID_T   int64_t
 #define AGI_GID_FT  C_INT64_T
-#define AGI_LID_T   int32_t
-#define AGI_LID_FT  C_INT32_T
+#define AGI_LID_T   LOCAL_ID_TYPE
+#define AGI_LID_FT  LOCAL_ID_FTYPE
 #define AGI_WGT_T   double
 #define AGI_WGT_FT  C_DOUBLE
 #define AGI_PART_T  int32_t
 #define AGI_PART_FT C_INT32_T
-#define AGI_EDGE_T  int32_t
-#define AGI_EDGE_FT C_INT32_T
+#define AGI_EDGE_T  LOCAL_ID_TYPE
+#define AGI_EDGE_FT LOCAL_ID_FTYPE
 
 #endif
