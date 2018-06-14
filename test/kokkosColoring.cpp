eaaf5168fb7db750d0970242812dbb5294ab48c7
Lucas Davis <davisl3@rpi.edu>
2018-06-14 16:06:57 -0400

    added comments for kokkos kernel

--- test/kokkosColoring.cpp
+++ test/kokkosColoring.cpp
@@ -23,13 +23,13 @@ int main(int argc, char* argv[]) {
 
   agi::PNgraph* pg = g->publicize();
 
-  agi::lid_t numverts = pg->num_local_verts;
+  const agi::lid_t numverts = pg->num_local_verts;
 
-  agi::lid_t numedges = pg->num_local_edges[0];  // Add support for multiple edge types
+  const agi::lid_t numedges = pg->num_local_edges[0];  // Add support for multiple edge types
 
-  agi::lid_t* degree_list = pg->degree_list[0]; // ^
+  const agi::lid_t* degree_list = pg->degree_list[0]; // ^
 
-  agi::lid_t* edge_list = pg->edge_list[0]; // ^
+  const agi::lid_t* edge_list = pg->edge_list[0]; // ^
 
   // Create views for each part of the graph data-structure
   
@@ -38,6 +38,7 @@ int main(int argc, char* argv[]) {
   Kokkos::View<agi::lid_t*> edge_view ("edge_view",numedges);
  
   // Use parllel loops to fill the views
+  
   Kokkos::parallel_for(numverts+1, KOKKOS_LAMBDA( const int i ) {
     degree_view(i) = degree_list[i];
   });
@@ -45,6 +46,15 @@ int main(int argc, char* argv[]) {
   Kokkos::parallel_for(numedges, KOKKOS_LAMBDA( const int i) {
     edge_view(i) = edge_list[i];
   });
+
+  // KokkosKernels::KokkosSparse::CrsMatrix<const agi::lid_t, ?, device>(numverts,numverts,numedges,degree_view,edge_view);
+
+  // KokkosKernelsHandle<const Kokkos::View<agi::lid_t,const Kokkos::View<agi::lid_t*>,const agi::lid_t> *kh = new KernelHandle;
+  // kh.set_team_work_size(16);
+  // kh.set_dynamic_scheduling(true);
+  // kh.create_graph_coloring_handle(COLORING_SERIAL);
+  //
+  // kh->destroy_graph_coloring_handle();
 
   destroyGraph(g);
   PCU_Barrier();
