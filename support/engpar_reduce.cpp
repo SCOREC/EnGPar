87dd3816524a60e6edd5b78ca9725a959790ca51
Cameron Smith <smithc11@rpi.edu>
2019-03-07 09:54:41 -0500

    reduction helper functions

---
+++ support/engpar_reduce.cpp
@@ -0,0 +1,15 @@
+#include "engpar_reduce.h"
+namespace engpar {
+  typedef ENGPAR_LID_T lid_t;
+  lid_t getMax(LIDs a) {
+    lid_t max;
+    const int n = a.dimension_0();
+    Kokkos::parallel_reduce(n, KOKKOS_LAMBDA(const int i, lid_t& lmax) {
+      const lid_t val = a(i);
+      if(val>lmax) {
+        lmax = val;
+      }
+    },Kokkos::Max<lid_t>(max));
+    return max;
+  }
+}
