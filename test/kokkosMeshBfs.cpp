a98e09eaf2c68143bbd450447558fd644a39d8f3
Cameron Smith <smithc11@rpi.edu>
2019-11-27 09:38:05 -0500

    remove bfs without component support

--- test/kokkosMeshBfs.cpp
+++ test/kokkosMeshBfs.cpp
@@ -28,79 +28,6 @@ apf::Field* convert_my_tag(apf::Mesh* m, apf::MeshTag* t, std::string name) {
   return f;
 }
 
-agi::lid_t* bfs(agi::Ngraph* g, agi::lid_t t, std::vector<int> start_vs) {
-  assert(g->isHyper());
-  agi::PNgraph* pg = g->publicize();
-  g->parallel_create_eve(t);
-  agi::lid_t numEnts = pg->num_local_edges[t];
-  //transfer the eve to the device
-  LIDs offsets_d("offsets_d", numEnts+1);
-  hostToDevice(offsets_d, pg->eve_offsets[t]);
-  printf("%d numEnts %d size of lists %d\n",
-      PCU_Comm_Self(), numEnts, pg->eve_offsets[t][numEnts]);
-  LIDs lists_d("lists_d", pg->eve_offsets[t][numEnts]);
-  hostToDevice(lists_d, pg->eve_lists[t]);
-  //create a device array for the visited flag and initialize it
-  LIDs visited_d("visited_d", numEnts);
-  LIDs visited_next_d("visited_next_d", numEnts);
-  //create a device array for the distance and initialize it
-  LIDs dist_d("dist_d", numEnts);
-  Kokkos::parallel_for(numEnts, KOKKOS_LAMBDA(const int e) {
-    visited_d(e) = 0;
-    visited_next_d(e) = 0;
-    dist_d(e) = 0;
-  });
-  //label starting vertices
-  agi::lid_t numStarts = static_cast<int>(start_vs.size());
-  //convert std::vector to agi::lid_t array for hostToDevice
-  agi::lid_t *starts = (agi::lid_t*)calloc(numStarts, sizeof(agi::lid_t));
-  for (int i = 0; i < numStarts; i++) {
-    starts[i] = start_vs[i];
-  }
-  LIDs starts_d("starts_d", numStarts);
-  hostToDevice(starts_d, starts);
-  Kokkos::parallel_for(numStarts, KOKKOS_LAMBDA(const int e) {
-    visited_next_d(starts_d(e)) = 1;
-  });
-  free(starts);
-  //create a flag to indicate if any distances have changed
-  agi::lid_t found = 0;
-  LIDs found_d("found_d", 1);
-  //count iterations
-  int iter = 0;
-  int max_iter = 1000;
-  LIDs iter_d("iter_d", 1);
-  //run bfs
-  do {
-    found = 0; // reset the found flag
-    iter++;  // increment the iteration count
-    hostToDevice(iter_d, &iter);
-    hostToDevice(found_d, &found);
-    //
-    // insert bfs code here
-    Kokkos::parallel_for(numEnts, KOKKOS_LAMBDA(const int u) {
-      visited_d(u) |= visited_next_d(u);
-      visited_next_d(u) = 0;
-      // loop over adjacent vertices
-      for (int adj = offsets_d(u); adj < offsets_d(u+1); adj++) {
-        int v = lists_d(adj);
-        visited_next_d(u) |= visited_d(v);
-      }
-      dist_d(u) = dist_d(u) + ((visited_next_d(u) && !visited_d(u)) * iter_d(0));
-      // set the flag to indicate that a new vertex was found 
-      if (visited_next_d(u) && !visited_d(u)) found_d(0) = 1;
-    });
-    deviceToHost(found_d, &found);
-    //
-  } while (found && iter < max_iter);
-  //transfer the distance to the host
-  fprintf(stderr, "\n%d total iterations %d\n", PCU_Comm_Self(), iter);
-  agi::lid_t* dist = new agi::lid_t[numEnts];
-  deviceToHost(dist_d, dist);
-  return dist;
-}
-
-
 // bfs for getting the depths from starts
 void bfs_depth(LIDs &offsets_d, LIDs &lists_d, LIDs &dist_d, std::vector<int> start_vs, agi::lid_t numEnts) {
   //create a device array for the visited flag and initialize it
@@ -376,22 +303,6 @@ int main(int argc, char* argv[]) {
   m->end(vitr);
   convert_my_tag(m,compDistTag,"component_distance");
   free(compDist);
-
-  // run bfs
-  agi::lid_t* distance = bfs(g,edges[0],{ 0, 20, 40 });
-  // tag mesh vertices with bfs distance
-  apf::MeshTag* dist = m->createIntTag("distance",1);
-  vitr = m->begin(0);
-  i=0;
-  while ((ent = m->iterate(vitr))) {
-    int d = distance[i];
-    m->setIntTag(ent,dist,&d);
-    i++;
-  }
-  m->end(vitr);
-  convert_my_tag(m, dist, "bfs_distance");
-  apf::writeVtkFiles(argv[3], m);
-  free(distance);
 
   destroyGraph(g);
   PCU_Barrier();
